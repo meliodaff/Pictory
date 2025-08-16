57242505e8d6deb9f4ce348140ea57e6dcaad3c8
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2025-08-16 15:25:54 +0800

    Update handleFileChange.tsx

--- Frontend/src/services/handleFileChange.tsx
+++ Frontend/src/services/handleFileChange.tsx
@@ -42,9 +42,7 @@ const useHandleFileChange = (
     }, 5000);
     try {
       const response = await axios.post(
-        "http://13.213.34.139:3000",
-        // "http://13.213.34.139:3000",
-        // "https://api-pictory.onrender.com",
+        "https://api-pictory.onrender.com",
         // "http://localhost:3000",
         formData,
         {
